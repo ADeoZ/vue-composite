cf075a84f85d182249a6a106fd407f3c146ac20b
Aleksey Denisov <deoz@yandex.ru>
2023-02-28 22:31:09 +0300

    form container

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,28 @@
     <aside class="controls">
       <header class="controls__title"><h2>Добавление товара</h2></header>
       <form class="form_add-item">
-        <label
+        <label class="form__label"
           >Наименование товара<input
             type="text"
             name="item-name"
             placeholder="Введите наименование товара"
             class="form__input"
         /></label>
-        <label
+        <label class="form__label"
           >Описание товара<textarea
             name="item-description"
             placeholder="Введите описание товара"
             class="form__textarea"
           ></textarea>
         </label>
-        <label
+        <label class="form__label"
           >Ссылка на изображение товара<input
             type="text"
             name="item-image"
             placeholder="Введите ссылку"
             class="form__input"
         /></label>
-        <label
+        <label class="form__label"
           >Цена товара<input type="text" name="item-price" placeholder="Введите цену" class="form__input"
         /></label>
         <button type="submit">Добавить товар</button>
@@ -41,10 +41,13 @@
       <ul class="catalog__list">
         <li class="catalog__item">
           <div class="item__image">
-            <img src="@/assets/images/photo.png" alt="Фото товара">
+            <img src="@/assets/images/photo.png" alt="Фото товара" />
           </div>
           <header class="item__name">Наименование товара</header>
-          <div class="item__description">Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк</div>
+          <div class="item__description">
+            Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание
+            товара в несколько строк
+          </div>
           <div class="item__price">10 000 руб.</div>
           <div class="item__delete"><button></button></div>
         </li>
@@ -61,4 +64,30 @@ export default {
 
 <style lang="scss">
 @import "@/assets/scss/style.scss";
+
+.main {
+  @include flex(left);
+  padding: 0 2rem;
+}
+
+.controls {
+  @include flex;
+  flex-direction: column;
+  max-width: 20.75rem;
+}
+
+.form_add-item {
+  padding: 1.5rem;
+  @include card;
+}
+
+.form {
+  &__label {
+    color: $font-form;
+    font-weight: 400;
+    font-size: 0.625rem;
+    line-height: 0.8125rem;
+    letter-spacing: -0.02em;
+  }
+}
 </style>
