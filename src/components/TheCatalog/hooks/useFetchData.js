152d9521e04ef89ad5b1a00a7ded32f56cd2160f
Aleksey Denisov <deoz@yandex.ru>
2023-03-29 21:26:08 +0300

    useFetchData refactoring

--- src/components/TheCatalog/hooks/useFetchData.js
+++ src/components/TheCatalog/hooks/useFetchData.js
@@ -1,32 +1,27 @@
 import { ref, watch } from "vue";
 
 export const useFetchData = (fetchMethod, localStorageKey) => {
-  const data = ref([]);
+  const localData = JSON.parse(localStorage.getItem(localStorageKey));
+  const data = ref(localData ?? []);
+  watch(data, () => {
+    localStorage.setItem(localStorageKey, JSON.stringify(data.value));
+  });
+
   const isLoading = ref(false);
 
   const fetchData = async () => {
-    const localData = JSON.parse(localStorage.getItem(localStorageKey));
-    if (localData) {
-      data.value = localData;
-      return;
-    }
+    if (localData) return;
 
     try {
       isLoading.value = true;
       const response = await fetchMethod();
       data.value = response.data;
-
-      localStorage.setItem(localStorageKey, JSON.stringify(response.data));
     } catch (e) {
       console.error("Ошибка загрузки");
     } finally {
       isLoading.value = false;
     }
   };
-
-  watch(data, () => {
-    localStorage.setItem(localStorageKey, JSON.stringify(data.value));
-  });
 
   return { fetchData, data, isLoading };
 };
