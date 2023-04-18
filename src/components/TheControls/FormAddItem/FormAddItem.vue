cf218cdaf46984728b4d40fb4b811de7d128cf32
Aleksey Denisov <deoz@yandex.ru>
2023-04-18 21:21:35 +0300

    addItem

--- src/components/TheControls/FormAddItem/FormAddItem.vue
+++ src/components/TheControls/FormAddItem/FormAddItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="form_add-item">
+  <form class="form_add-item" @submit.prevent="submitForm">
     <v-text-field
       v-model="itemsForm.name"
       v-model:error="errorsForm.name"
@@ -40,10 +40,14 @@ import { computed, ref } from "vue";
 import { validations, format } from "@/components/TheControls/FormAddItem/helpers";
 import VTextField from "@/components/VTextField";
 import VButton from "@/components/VButton";
+import { useFetchData } from "@/components/hooks";
 
 const itemsForm = ref({ name: "", description: "", image: "", price: "" });
 const errorsForm = ref({ name: true, image: true, price: true });
 const isValidForm = computed(() => Object.values(errorsForm.value).some(Boolean));
+
+const { addItem } = useFetchData();
+const submitForm = () => addItem(itemsForm);
 </script>
 
 <style lang="scss" scoped>
--- src/components/hooks/useFetchData.js
+++ src/components/hooks/useFetchData.js
@@ -1,19 +1,34 @@
 import { ref, watch, onMounted } from "vue";
 import { getItemList } from "@/api/catalogAPI";
 
-const fetchMethod = getItemList;
-const localStorageKey = "catalog";
+const fetchMethod = getItemList; // api method
+const localStorageKey = "catalog"; // localStorage key to store items
 
 const localData = JSON.parse(localStorage.getItem(localStorageKey));
 const itemList = ref(localData ?? []);
 
+// store items in storage
+watch(itemList, () => {
+  localStorage.setItem(localStorageKey, JSON.stringify(itemList.value));
+});
+
+// очистка формы
+// проверка loading на кнопку формы
+// тест на очень крупные значения и длинные описания
+// статус успешного добавления
+// переписать метод
+// add item
+const addItem = (itemData) => {
+  const lastId = itemList.value[itemList.value.length - 1].id;
+  itemList.value = [...itemList.value, {...itemData, price: +itemData.price.replace(/\s/g, ""), id: lastId + 1}];
+  console.log(itemList.value);
+}
+
+// delete item
 const deleteItem = (deleteId) => (itemList.value = itemList.value.filter((item) => item.id !== deleteId));
 
+// main hook method to fetch data through api
 export const useFetchData = () => {
-  watch(itemList, () => {
-    localStorage.setItem(localStorageKey, JSON.stringify(itemList.value));
-  });
-
   const isLoading = ref(false);
 
   const fetchData = async () => {
@@ -32,5 +47,5 @@ export const useFetchData = () => {
 
   onMounted(fetchData);
 
-  return { itemList, isLoading, deleteItem };
+  return { itemList, isLoading, addItem, deleteItem };
 };
